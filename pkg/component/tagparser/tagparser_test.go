abe317736526df4def898253e3b0c8898be61f64
Andriy <andriy.savchenko@gmail.com>
2025-12-10 12:05:54 -0800

    Use testify for tagparser tests

--- pkg/component/tagparser/tagparser_test.go
+++ pkg/component/tagparser/tagparser_test.go
@@ -2,8 +2,10 @@ package tagparser
 
 import (
 	"errors"
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type M = map[string]string
@@ -73,65 +75,26 @@ func TestParse(t *testing.T) {
 		t.Run(test.testName, func(t *testing.T) {
 			tag, err := Parse(test.tag)
 
-			checkError(t, test.tag, err, test.error)
-			if err != nil {
-				return
+			if test.error != "" {
+				require.Error(t, err)
+				assert.Equal(t, test.error, err.Error())
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, test.name, tag.Name)
+				if test.opts == nil {
+					assert.Empty(t, tag.Options)
+				} else {
+					assert.Equal(t, test.opts, tag.Options)
+				}
 			}
-
-			checkName(t, test.tag, tag.Name, test.name)
-			checkOptions(t, test.tag, tag.Options, test.opts)
 		})
-	}
-}
-
-func checkError(t *testing.T, tag string, err error, wantErr string) {
-	t.Helper()
-	if err != nil {
-		ae := err.Error()
-		if wantErr == "" {
-			t.Errorf("** Parse(%q) error %q, wanted no error", tag, ae)
-		} else if ae != wantErr {
-			t.Errorf("** Parse(%q) error %q, wanted error %q", tag, ae, wantErr)
-		}
-	} else if wantErr != "" {
-		t.Errorf("** Parse(%q) no error, wanted error %q", tag, wantErr)
-	}
-}
-
-func checkName(t *testing.T, tag string, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("** Parse(%q) name = %q, wanted %q", tag, got, want)
-	}
-}
-
-func checkOptions(t *testing.T, tag string, got, want map[string]string) {
-	t.Helper()
-	if reflect.DeepEqual(got, want) {
-		return
-	}
-
-	for k, ev := range want {
-		av, ok := got[k]
-		if !ok {
-			t.Errorf("** Parse(%q) missing option %q = %q", tag, k, ev)
-		} else if av != ev {
-			t.Fatalf("** Parse(%q) option %q = %q, wanted %q", tag, k, av, ev)
-		}
-	}
-	for k, av := range got {
-		_, ok := want[k]
-		if !ok {
-			t.Errorf("** Parse(%q) extra option %q = %q", tag, k, av)
-		}
 	}
 }
 
 func TestParse_duplicate(t *testing.T) {
 	_, err := Parse(`foo:bar,foo:boz`)
-	if !errors.Is(err, ErrDuplicateKey) {
-		t.Errorf("** err = %v, wanted %v", err, ErrDuplicateKey)
-	}
+	require.Error(t, err)
+	assert.True(t, errors.Is(err, ErrDuplicateKey))
 }
 
 var errSimulated = errors.New("simulated error")
@@ -146,13 +109,12 @@ func TestParseFunc_custom_error_in_name(t *testing.T) {
 
 		return nil
 	})
-	if err.Error() != expErr {
-		t.Errorf("** error = %v, wanted %v", err, expErr)
-	}
+	require.Error(t, err)
+	assert.Equal(t, expErr, err.Error())
+
 	var errType *Error
-	if !errors.As(err, &errType) || !errors.Is(errType.Cause, errSimulated) {
-		t.Errorf("** Cause = %v, wanted %v", err, expErr)
-	}
+	require.True(t, errors.As(err, &errType))
+	assert.True(t, errors.Is(errType.Cause, errSimulated))
 }
 
 func TestParseFunc_custom_error_in_key(t *testing.T) {
@@ -165,18 +127,17 @@ func TestParseFunc_custom_error_in_key(t *testing.T) {
 
 		return nil
 	})
-	if err.Error() != expErr {
-		t.Errorf("** error = %v, wanted %v", err, expErr)
-	}
+	require.Error(t, err)
+	assert.Equal(t, expErr, err.Error())
+
 	var errType *Error
-	if !errors.As(err, &errType) || !errors.Is(errType.Cause, errSimulated) {
-		t.Errorf("** Cause = %v, wanted %v", err, expErr)
-	}
+	require.True(t, errors.As(err, &errType))
+	assert.True(t, errors.Is(errType.Cause, errSimulated))
 }
 
 func BenchmarkParseFunc(t *testing.B) {
 	slice := make([]string, 0, 20)
-	for i := 0; i < t.N; i++ {
+	for t.Loop() {
 		slice = slice[:0]
 		err := ParseFunc(`foo,bar:boz,fubar,bar:zob,oof`, func(key, value string) error {
 			slice = append(slice, key, value)
